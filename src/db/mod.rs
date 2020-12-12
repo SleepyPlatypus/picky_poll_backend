e9f7a42f9639c67db6401b14b7c3ae9c38d56120
Stephen <stephenmerc@gmail.com>
2020-12-12 06:09:20 +0000

    WIP; test loads

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,14 +2,19 @@ use chrono::{
     DateTime,
     offset::Utc,
 };
-use tokio_postgres::Client;
+use sqlx::{
+    Connection,
+    Executor,
+    postgres::PgPool,
+};
 
 pub struct VoteDbClient {
-    postgres: Client,
+    db: PgPool,
 }
 
 type Timestamp = DateTime<Utc>;
 
+#[derive(sqlx::FromRow)]
 pub struct Poll {
     pub id: String,
     pub name: String,
@@ -19,59 +24,111 @@ pub struct Poll {
     pub close: Option<Timestamp>,
 }
 
+#[derive(Debug)]
 pub enum PutPollErr {
-    PostgresErr(tokio_postgres::Error),
+    PostgresErr(sqlx::Error),
     Conflict,
 }
 
-impl From<tokio_postgres::Error> for PutPollErr {
-    fn from(e: tokio_postgres::Error) -> PutPollErr {
+impl From<sqlx::Error> for PutPollErr {
+    fn from(e: sqlx::Error) -> PutPollErr {
         PutPollErr::PostgresErr(e)
     }
 }
 
 pub enum GetPollErr {
-    PostgresErr(tokio_postgres::Error),
+    PostgresErr,
     NotFound(String),
 }
-impl From<tokio_postgres::Error> for GetPollErr {
-    fn from(e: tokio_postgres::Error) -> GetPollErr {
-        GetPollErr::PostgresErr(e)
+
+
+impl From<sqlx::Error> for GetPollErr {
+    fn from(e: sqlx::Error) -> GetPollErr {
+        GetPollErr::PostgresErr
     }
 }
 
-impl VoteDbClient {
-    pub async fn put_poll(self: &VoteDbClient, poll: &Poll) -> Result<(), PutPollErr> {
-        self.postgres
-            .query(
-                "insert into poll(id, name, description, ownerId, expires, close)
-                values ($1, $2, $3, $4, $5, $6)",
-                &[&poll.id, &poll.name, &poll.description, &poll.owner_id, &poll.expires, &poll.close]
-            )
-            .await?;
-        Ok(())
-    }
+pub async fn put_poll(voteDb: &VoteDbClient, poll: &Poll) -> Result<(), PutPollErr> {
 
-    pub async fn get_poll(self: &VoteDbClient, id: &str) -> Result<Poll, GetPollErr> {
-        let rows = self
-            .postgres
-            .query(
-                "select id, name, description, ownerId, expires, close from poll where id=$1",
-                &[&id],
-            )
-            .await?;
+    let mut query = sqlx::query("
+insert
+into poll(id, name, description, ownerId, expires, close)
+values ($1, $2, $3, $4, $5, $6)")
+        .bind(&poll.id)
+        .bind(&poll.name)
+        .bind(&poll.description)
+        .bind(&poll.owner_id)
+        .bind(&poll.expires)
+        .bind(&poll.close);
+    
+    voteDb.db.execute(query).await?;
+    Ok(())
+}
 
-        let row = rows.get(0).ok_or(GetPollErr::NotFound(String::from(id)))?;
+// impl<'e, E> VoteDbClient<'e, E>
+// where E: Executor<'e> {
+//     pub async fn put_poll(self: &VoteDbClient<'e, E>, poll: &Poll) -> Result<(), PutPollErr> {
+//         let query = sqlx::query("
+//             INSERT into poll(id, name, description, ownerId, expires, close)
+//             values (?, ?, ?, ?, ?, ?)
+//         ").bind(poll.id)
+//         .bind(poll.name)
+//         .bind(poll.description)
+//         .bind(poll.owner_id)
+//         .bind(poll.expires)
+//         .bind(poll.close).execute(self.db).await?;
+//         Ok(())
+//     }
+    // pub async fn get_poll(self: &VoteDbClient<'c, E>, id: &str) -> Result<Poll, GetPollErr> {
+    //     let rows = sqlx::query(
+    //         "select id, name, description, ownerId, expires, close from poll where id=?",
+    //     ).bind(id).execute(self.db).await?;
 
-        let poll = Poll {
-            id: row.get(0),
-            name: row.get(1),
-            description: row.get(2),
-            owner_id: row.get(3),
-            expires: row.get(4),
-            close: row.get(5),
+    //     let row = rows.get(0).ok_or(GetPollErr::NotFound(String::from(id)))?;
+
+    //     let poll = Poll {
+    //         id: row.get(0),
+    //         name: row.get(1),
+    //         description: row.get(2),
+    //         owner_id: row.get(3),
+    //         expires: row.get(4),
+    //         close: row.get(5),
+    //     };
+
+    //     Ok(poll)
+    // }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        any::Any,
+        env,
+    };
+    use sqlx::postgres::PgPoolOptions;
+
+    const DATABASE_URL: &str = "DATABASE_URL";
+
+    #[tokio::test]
+    async fn test_put_poll() -> Result<(), PutPollErr> {
+        let db_url = &env::var(&DATABASE_URL).unwrap();
+        let pool = PgPoolOptions::new()
+            .max_connections(1)
+            .connect(db_url)
+            .await
+            .unwrap();
+
+        let client = VoteDbClient{db: pool};
+        let mockPollRow = Poll {
+            id: String::from(""),
+            name: String::from("My poll"),
+            description: String::from("what a great poll"),
+            owner_id: String::from("A"),
+            close: None,
+            expires: Utc::now(),
         };
-
-        Ok(poll)
+        put_poll(&client, &mockPollRow).await?;
+        Ok(())
     }
 }
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,53 +1,56 @@
-use chrono::{
-    DateTime,
-    Duration,
-    offset::Utc,
-};
-use rand::{
-    distributions::Alphanumeric,
-    thread_rng,
-    Rng
-};
-use super::db::{
-    self,
-    VoteDbClient,
-};
-struct PostPollRequest {
-    name: String,
-    description: String,
-    expires: DateTime<Utc>,
-    close: DateTime<Utc>,
-}
+// use chrono::{
+//     DateTime,
+//     Duration,
+//     offset::Utc,
+// };
+// use rand::{
+//     distributions::Alphanumeric,
+//     thread_rng,
+//     Rng
+// };
+// use sqlx::Connection;
+// use super::db::{
+//     self,
+//     VoteDbClient,
+// };
+// struct PostPollRequest {
+//     name: String,
+//     description: String,
+//     expires: DateTime<Utc>,
+//     close: DateTime<Utc>,
+// }
 
-struct PollService {
-    db: VoteDbClient,
-}
+// struct PollService<C>
+// where C: Connection {
+//     db: VoteDbClient<C>,
+// }
 
-enum PostPollError {
-    Conflict,
-    Error(tokio_postgres::Error),
-}
+// enum PostPollError {
+//     Conflict,
+//     Error,
+// }
 
-impl From<db::PutPollErr> for PostPollError {
-    fn from(e : db::PutPollErr) -> Self {
-        match e {
-            db::PutPollErr::PostgresErr(e) => PostPollError::Error(e),
-            db::PutPollErr::Conflict => PostPollError::Conflict,
-        }
-    }
-}
+// impl From<db::PutPollErr> for PostPollError {
+//     fn from(e : db::PutPollErr) -> Self {
+//         match e {
+//             db::PutPollErr::PostgresErr => PostPollError::Error,
+//             db::PutPollErr::Conflict => PostPollError::Conflict,
+//         }
+//     }
+// }
 
-impl PollService {
-    async fn post_poll(&self, request: PostPollRequest, id: String) -> Result<String, PostPollError> {
-        let poll_row = db::Poll {
-            id: thread_rng().sample_iter(&Alphanumeric).take(10).collect(),
-            name: request.name,
-            description: request.description,
-            owner_id: id,
-            expires: Utc::now() + Duration::days(7),
-            close: None,
-        };
-        self.db.put_poll(&poll_row).await?;
-        Ok(poll_row.id)
-    }
-}
+// impl<C> PollService<C>
+// where C: Connection {
+//     async fn post_poll(&self, request: PostPollRequest, id: String) -> Result<String, PostPollError> {
+//         let poll_row = db::Poll {
+//             id: thread_rng().sample_iter(&Alphanumeric).take(10).collect(),
+//             name: request.name,
+//             description: request.description,
+//             owner_id: id,
+//             expires: Utc::now() + Duration::days(7),
+//             close: None,
+//         };
+//         self.db.put_poll(&poll_row).await?;
+//         Ok(poll_row.id)
+//     }
+// }
