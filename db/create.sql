73047fa564d2ecff37bd68530421e10f2e911eb0
Stephen Mercado <stephenmerc@gmail.com>
2021-02-09 23:23:56 +0000

    Add upsert method to db layer

--- db/create.sql
+++ db/create.sql
@@ -12,7 +12,6 @@ CREATE TABLE poll
 CREATE INDEX expires_index ON poll USING btree
     (expires ASC NULLS LAST);
 
-
 --CANDIDATE--
 CREATE TABLE candidate
 (
@@ -25,14 +24,37 @@ CREATE TABLE candidate
     poll_id character varying NOT NULL
 );
 
-ALTER TABLE public.candidate
+ALTER TABLE candidate
     ADD CONSTRAINT candidate_poll_fkey FOREIGN KEY (poll_id)
-    REFERENCES public.poll (id) MATCH SIMPLE
+    REFERENCES poll (id)
     ON UPDATE RESTRICT
     ON DELETE CASCADE;
 
 CREATE INDEX fki_candidate_poll_fkey
-    ON public.candidate(poll_id);
+    ON candidate(poll_id);
 
-ALTER TABLE public.candidate
+ALTER TABLE candidate
     ADD UNIQUE (name, poll_id);
+
+--BALLOT--
+CREATE TABLE ballot
+(
+    id character varying NOT NULL,
+
+    name character varying NOT NULL,
+    timestamp timestamp with time zone NOT NULL,
+    owner_id character varying NOT NULL,
+
+    poll_id character varying NOT NULL,
+
+    CONSTRAINT ballot_pkey PRIMARY KEY (id, poll_id)
+);
+
+ALTER TABLE ballot
+    ADD CONSTRAINT ballot_poll_fkey FOREIGN KEY (poll_id)
+    REFERENCES poll (id)
+    ON UPDATE RESTRICT
+    ON DELETE CASCADE;
+
+CREATE INDEX fki_ballot_poll_fkey
+    ON ballot(poll_id);
--- src/db.rs
+++ src/db.rs
@@ -58,6 +58,28 @@ pub enum InsertCandidateErr {
     PostgresErr(sqlx::Error),
 }
 
+#[derive(Debug)]
+pub enum UpsertBallotErr {
+    Conflict,
+    NotAuthorized,
+    PostgressErr(sqlx::Error),
+}
+
+impl UpsertBallotErr {
+    pub fn postgres(e: sqlx::Error) -> UpsertBallotErr {
+        UpsertBallotErr::PostgressErr(e)
+    }
+}
+
+#[derive(sqlx::FromRow, Debug, Eq, PartialEq)]
+#[cfg_attr(test, derive(Clone))]
+pub struct Ballot {
+    pub id: String,
+    pub name: String,
+    pub timestamp: Timestamp,
+    pub owner_id: String,
+}
+
 impl From<sqlx::Error> for InsertCandidateErr {
     fn from(e: sqlx::Error) -> InsertCandidateErr {
         if e.as_database_error()
@@ -75,35 +97,6 @@ impl From<sqlx::Error> for InsertCandidateErr {
 impl PickyDb {
     pub fn new(db_pool: PgPool) -> PickyDb {
         PickyDb{ pool: db_pool }
-    }
-
-    pub async fn insert_candidates(&self, poll_id: &str, candidates: &Vec<Candidate>) -> Result<(), InsertCandidateErr> {
-        let mut tx = self.pool.begin()
-            .await?;
-
-        for candidate in candidates {
-            let query = sqlx::query(
-                "insert \
-                into candidate(name, description, poll_id) \
-                    values ($1, $2, $3)")
-                .bind(&candidate.name)
-                .bind(&candidate.description)
-                .bind(poll_id);
-            query.execute(&mut tx).await?;
-        };
-        tx.commit().await?;
-        Ok(())
-    }
-
-    pub async fn select_candidates(&self, poll_id: &str) -> Result<Vec<Candidate>, sqlx::Error> {
-        let mut tx = self.pool.begin()
-            .await
-            ?;
-        let query = sqlx::query_as::<_, Candidate>(
-            "select name, description from candidate where poll_id = $1"
-        ).bind(poll_id);
-
-        Ok(query.fetch_all(&mut tx).await?)
     }
 
     pub async fn insert_poll(&self, poll: &Poll) -> Result<(), InsertPollErr>
@@ -133,6 +126,77 @@ impl PickyDb {
         let poll = query.fetch_optional(&self.pool).await?;
 
         poll.ok_or(SelectPollErr::NotFound)
+    }
+
+    pub async fn insert_candidates(&self, poll_id: &str, candidates: &Vec<Candidate>) -> Result<(), InsertCandidateErr> {
+        let mut tx = self.pool.begin()
+            .await?;
+
+        for candidate in candidates {
+            let query = sqlx::query(
+                "insert \
+                into candidate(name, description, poll_id) \
+                    values ($1, $2, $3)")
+                .bind(&candidate.name)
+                .bind(&candidate.description)
+                .bind(poll_id);
+            query.execute(&mut tx).await?;
+        };
+        tx.commit().await?;
+        Ok(())
+    }
+
+    pub async fn select_candidates(&self, poll_id: &str) -> Result<Vec<Candidate>, sqlx::Error> {
+        let mut tx = self.pool.begin()
+            .await?;
+        let query = sqlx::query_as::<_, Candidate>(
+            "select name, description from candidate where poll_id = $1"
+        ).bind(poll_id);
+
+        Ok(query.fetch_all(&mut tx).await?)
+    }
+
+    pub async fn upsert_ballot(&self, poll_id: &str, ballot: Ballot) -> Result<(), UpsertBallotErr> {
+        let get = sqlx::query_as::<_, Ballot>(
+            "select id, name, timestamp, owner_id from ballot where id = $1 and poll_id=$2"
+        ).bind(&ballot.id)
+        .bind(poll_id);
+
+        let mut tx = self.pool.begin().await.map_err(UpsertBallotErr::postgres)?;
+        let previous_row = get.fetch_optional(&mut tx)
+            .await.map_err(UpsertBallotErr::postgres)?;
+        
+        match previous_row {
+            None => {
+                let insert = sqlx::query(
+                    "insert into ballot(id, name, timestamp, owner_id, poll_id) values ($1, $2, $3, $4, $5)"
+                ).bind(&ballot.id)
+                .bind(&ballot.name)
+                .bind(&ballot.timestamp)
+                .bind(&ballot.owner_id)
+                .bind(poll_id);
+                insert.execute(&mut tx)
+                    .await.map_err(UpsertBallotErr::postgres)?;
+            },
+            Some(previous_row) => {
+                if previous_row.owner_id != ballot.id {
+                    return Err(UpsertBallotErr::NotAuthorized);
+                } else if previous_row.name != ballot.name {
+                    return Err(UpsertBallotErr::Conflict);
+                } else {
+                    let update = sqlx::query(
+                        "update ballot(timestamp) where id = $1 and poll_id = $2 values ($3)"
+                    ).bind(&ballot.id)
+                    .bind(poll_id)
+                    .bind(&ballot.timestamp);
+                    update.execute(&mut tx)
+                        .await
+                        .map_err(UpsertBallotErr::postgres)?;
+                }
+            },
+        }
+        tx.commit().await.map_err(UpsertBallotErr::postgres)?;
+        Ok(())
     }
 }
 
@@ -233,6 +297,27 @@ mod tests {
                 InsertCandidateErr::Conflict => (),
                 _ => panic!("Expected InsertCandidateErr {:?}", error),
             }
+        }
+    }
+
+    mod upsert_candidate {
+        use super::*;
+        #[tokio::test]
+        async fn happy_path_create() {
+            let client = PickyDb::new(test_db::new_pool().await);
+            let mock_poll_row = new_mock_poll();
+            client.insert_poll(&mock_poll_row).await.expect("Test setup failed to create poll");
+
+            let mock_ballot = Ballot {
+                id: "1".to_owned(),
+                name: "".to_owned(),
+                owner_id: "".to_owned(),
+                timestamp: Utc::now()
+            };
+
+            client.upsert_ballot(&mock_poll_row.id, mock_ballot)
+                .await
+                .expect("Should successfully create the ballot");
         }
     }
 }
